2e82022a4b557c605a7993f173a4aaa0aafae3c3
Mariano Molina <xsin.crueldadx@gmail.com>
2015-11-27 11:20:46 -0300

    Initial Commit

---
+++ MapBoxTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapBoxTestApp
+//
+//  Created by Mariano Molina on 27/11/15.
+//  Copyright © 2015 Funka.la. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
